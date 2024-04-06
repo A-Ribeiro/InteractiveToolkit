5093a3f662541dad5b07a6a3902035e8acc05802
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-04-06 13:36:44 -0300

    optimized fast arc

--- include/InteractiveToolkit/MathCore/FastCos.h
+++ include/InteractiveToolkit/MathCore/FastCos.h
@@ -12,27 +12,25 @@ namespace MathCore
 
     class FastArc
     {
+        static constexpr int number_of_samples_cos = 180 * 1000 * 4;
+        static constexpr float number_of_samples_cos_float = (float)number_of_samples_cos;
 
-        const int number_of_samples_cos = 180 * 1000 * 4;
-        const float number_of_samples_cos_float = (float)number_of_samples_cos;
 
-        // const float one_sample_above_1_0 = 1.0f + 1.0f / number_of_samples_cos_float;
+        static constexpr double FastArc_filter_on_x_d = 0.70710678118654752440084436210485;//OP<double>::cos(OP<double>::deg_2_rad(45.0));
+        static constexpr double FastArc_0_5_y_d = 0.29289321881345247559915563789515;//1.0 - OP<double>::sqrt(1.0 - FastArc_filter_on_x_d * FastArc_filter_on_x_d);
+        static constexpr double FastArc_new_inclination_d = FastArc_0_5_y_d / FastArc_filter_on_x_d;
+        static constexpr double FastArc_new_inclination_inv_d = FastArc_filter_on_x_d / FastArc_0_5_y_d;
 
-        // const int number_of_samples_sin = 90 * 1000 * 4;
-        // const float number_of_samples_sin_float = (float)number_of_samples_sin;
 
-        // const float one_sample_above_1_0_sin = 1.0f + 1.0f / number_of_samples_sin_float;
+        static constexpr float FastArc_filter_on_x_f = (float)FastArc_filter_on_x_d;
+        static constexpr float FastArc_0_5_y_f = (float)FastArc_0_5_y_d;
+        static constexpr float FastArc_new_inclination_f = (float)FastArc_new_inclination_d;
+        static constexpr float FastArc_new_inclination_inv_f = (float)FastArc_new_inclination_inv_d;
+
 
         std::vector<float> acos_values;
-        // std::vector<float> asin_values;
 
-        // number_of_samples_cos =>
-        //    90 degrees
-        //    times the number of places of precision (100) [you can query angles like these: 90.23]
-        //    times 2 (to double that precision)
-        //    = 90 * 100 * 2
         FastArc() : acos_values(number_of_samples_cos + 1)
-                    //,asin_values(number_of_samples_sin + 1)
         {
 
             for (int i = 0; i < number_of_samples_cos + 1; i++)
@@ -43,28 +41,16 @@ namespace MathCore
                 _cs = _cs * 2.0 - 1.0;
 
                 _cs = array_map_2_cos(_cs);
-                //_cs = cos_map_2_array(_cs);
 
                 _cs = OP<double>::clamp(_cs, -1.0, 1.0);
 
                 acos_values[i] = (float)::acos(_cs);
             }
             acos_values[number_of_samples_cos] = acos_values[number_of_samples_cos - 1];
-
-            // for (int i = 0; i < number_of_samples_sin + 1; i++)
-            // {
-            //     double _cs = (double)i / (double)number_of_samples_sin;
-            //     _cs = _cs * 2.0 - 1.0;
-
-            //     float sign = OP<double>::sign(_cs);
-            //     _cs = OP<double>::sqrt(OP<double>::abs(_cs)) * sign;
-
-            //     _cs = OP<double>::clamp(_cs, -1.0, 1.0);
-            //     asin_values[i] = (float)::asin(_cs);
-            // }
         }
 
     public:
+
         // [-1..1]
         static float ITK_INLINE array_map_2_cos(const double &_csp)
         {
@@ -73,49 +59,15 @@ namespace MathCore
             double sign_baixo_cima = OP<double>::sign(_csp);
 
             double _cs = OP<double>::abs(_csp);
-            //new new attempt
-            // https://www.desmos.com/calculator/heapv9nm20
-            //
-            //
-            // y = ((x+1)^16-1)/(65535)
-            //
-            // x = (65535 y + 1)^(1/16) - 1
-            // _cs = OP<float>::pow( 1.0 + 65535 * _cs, 1.0/16.0 ) - 1.0;
+            //double _cs = _csp;
 
+            // circ_sqrt
+            //_cs -= sign_baixo_cima;
 
-
-            //new attempt
-            // https://www.desmos.com/calculator/olxtedbbyk
-            //
-            // binomio base:
-            // x = sqrt(4 - 3 y) + 2         x = (16 - 15 y)^(1/4) + 2
-            //
-            // first case:
-            // y= (-(2-x)^2 + 1) / (2^2-1) + 1        y= (-(2-x)^4 + 1) / (2^4-1) + 1
-            //
-            double d = FastArc_DIM;
-            _cs = ( 1.0 - OP<double>::pow(2.0 - _cs, d) ) / ( OP<double>::pow(2.0, d) - 1.0 ) + 1.0;
-
-
-            // https://www.desmos.com/calculator/gun4vmvrf8
-            //
-            // binomio base:
-            // y = ((x+1)^d - 1)/(2^d-1)
-            //
-            // first case:
-            // 
-            // y = ((x+1)^3 - 1)/7
-            // x = -1 + (1 + 7 y)^(1/3)
-            //
-            // double d = 2.0;
-            //  _cs = OP<double>::pow(1.0 + (OP<double>::pow(2.0,d)-1.0) * _cs, 1.0/d) - 1.0;
-            //
-            // second case:
-            // 
-            // y = ((x+1)^4 - 1)/15
-            // x == (1 + 15 y)^(1/4) - 1
-            //
-            //_cs = OP<double>::pow(1.0 + 255.0 * _cs, 1.0/8.0) - 1.0;
+            double _circ = _cs - 1.0f;
+            _circ = OP<double>::sqrt(1.0f - _circ * _circ);
+            double _line = _cs * FastArc_new_inclination_inv_d;
+            _cs = (_cs > FastArc_0_5_y_d)?_circ:_line;
 
             // graph 0..1 to 0..0.5
             _cs *= sign_baixo_cima;
@@ -126,126 +78,30 @@ namespace MathCore
         // [-1..1]
         static float ITK_INLINE cos_map_2_array(const float &_csp)
         {
-
-            // [ < 0 => -1 baixo , > 0 => 1 cima ]
+        // [ < 0 => -1 baixo , > 0 => 1 cima ]
             float sign_baixo_cima = OP<float>::sign(_csp);
 
             float _cs = OP<float>::abs(_csp);
 
-            //new new attempt
-            // https://www.desmos.com/calculator/heapv9nm20
-            //
-            //
-            // y = ((x+1)^16-1)/(65535)
-            //
-            // x = (65535 y + 1)^(1/16) - 1
-            // float _cnst_ = 1.0f/65535.0f;
-            // _cs = _cs + 1.0f;
-            // _cs = _cs*_cs;// ^2
-            // _cs = _cs*_cs;// ^4
-            // _cs = _cs*_cs;// ^8
-            // _cs = _cs*_cs;// ^16
-            // _cs = (_cs-1.0f) * _cnst_;
+            // circ_sqrt
+            float _circ = 1.0f - OP<float>::sqrt(1.0f - _cs * _cs);
+            float _line = _cs * FastArc_new_inclination_f;
+            _cs = (_cs > FastArc_filter_on_x_f)?_circ:_line;
 
-            //new attempt
-            // https://www.desmos.com/calculator/olxtedbbyk
-            //
-            // binomio base:
-            // x = -sqrt(4 - 3 y) + 2         x = (16 - 15 y)^(1/4) + 2
-            //
-            // first case:
-            // y= (-(2-x)^2 + 1) / (2^2-1) + 1        y= (-(2-x)^4 + 1) / (2^4-1) + 1
-            // x = 2 - ((1 - 2^d) (2^d/(1 - 2^d) + y))^(1/d)
-            //
-            const float d = (float)FastArc_DIM;
-            const float one_over_d = 1.0f/d;
-            const float _2_power_d = OP<float>::pow(2.0f, d);
-            const float one_minus_2_power_d = 1.0f - _2_power_d;
-            const float _2_power_d_over_one_minus_2_power_d = _2_power_d / one_minus_2_power_d;
-
-            _cs = 2.0f - OP<float>::pow( one_minus_2_power_d * ( _2_power_d_over_one_minus_2_power_d + _cs ), one_over_d  );
-            //_cs = 2.0f - ::expf( ::logf( one_minus_2_power_d * ( _2_power_d_over_one_minus_2_power_d + _cs ) ) * one_over_d );
-
-            // _cs = OP<float>::sqrt( one_minus_2_power_d * ( _2_power_d_over_one_minus_2_power_d + _cs ) );
-            // _cs = OP<float>::sqrt(_cs); // 1/4
-            // _cs = OP<float>::sqrt(_cs); // 1/8
-            // _cs = OP<float>::sqrt(_cs); // 1/16
-            // _cs = 2.0f - _cs;
-
-            // https://www.desmos.com/calculator/gun4vmvrf8
-            //
-            // binomio base:
-            // y = ((x+1)^d - 1)/(2^d-1)
-            //
-            // first case:
-            //
-            // y = ((x+1)^3 - 1)/7
-            // x = -1 + (1 + 7 y)^(1/3)
-            // float _cs3 = _cs + 1;
-            // _cs3 = _cs3 * _cs3;
-            // const float _1_7 = 1.0f/3.0f;
-            // _cs = (_cs3 - 1.0f) * _1_7;
-            //
-            // second case:
-            // 
-            // y = ((x+1)^4 - 1)/15
-            // x == (1 + 15 y)^(1/4) - 1
-            //
-            // double _cs4 = _cs + 1;
-            // _cs4 = _cs4 * _cs4;
-            // _cs4 = _cs4 * _cs4;
-            // _cs4 = _cs4 * _cs4;
-            // const double _1_15 = 1.0f/255.0f;
-            // _cs = (float)((_cs4 - 1.0) * _1_15);
+            // if (_cs > FastArc_filter_on_x_f) {
+            //     float _circ = 1.0f - OP<float>::sqrt(1.0f - _cs * _cs);
+            //     _cs = _circ;
+            // } else {
+            //     float _line = _cs * FastArc_new_inclination_f;
+            //     _cs = _line;
+            // }
 
             // graph 0..1 to 0..0.5
             _cs *= sign_baixo_cima;
 
             return _cs;
+
         }
-
-        // // [-1..1]
-        // static double ITK_INLINE array_map_2_cos2(const double &_csp)
-        // {
-
-        //     // [ < 0 => -1 baixo , > 0 => 1 cima ]
-        //     double sign_baixo_cima = OP<double>::sign(_csp);
-        //     double _cs = OP<double>::abs(_csp) * 2.0f - 1.0f;
-        //     double _second_sign = OP<double>::sign(_cs);
-
-        //     // circ_sqrt
-        //     _cs = _second_sign - _cs;
-        //     _cs = OP<double>::sqrt(1.0f - _cs * _cs);
-        //     _cs *= _second_sign;
-
-        //     _cs = _cs + 1.0f;
-
-        //     // graph 0..1 to 0..0.5
-        //     _cs *= sign_baixo_cima * 0.5f;
-
-        //     return _cs;
-        // }
-
-        // // [-1..1]
-        // static float ITK_INLINE cos_map_2_array2(const float &_csp)
-        // {
-
-        //     // [ < 0 => -1 baixo , > 0 => 1 cima ]
-        //     float sign_baixo_cima = OP<float>::sign(_csp);
-        //     float _cs = OP<float>::abs(_csp) * 2.0f - 1.0f;
-        //     float _second_sign = OP<float>::sign(_cs);
-
-        //     // circ_sqrt
-        //     _cs = 1.0 - OP<float>::sqrt(1.0f - _cs * _cs);
-        //     _cs *= _second_sign;
-
-        //     _cs = _cs + 1.0f;
-
-        //     // graph 0..1 to 0..0.5
-        //     _cs *= sign_baixo_cima * 0.5f;
-
-        //     return _cs;
-        // }
 
         float ITK_INLINE acos(const float &_csp) const
         {
