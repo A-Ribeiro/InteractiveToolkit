c7998340ae1af806c94e9dabc7e154aba540788b
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-07-05 20:29:01 -0300

    new toFile from Directory

--- include/InteractiveToolkit/ITKCommon/FileSystem/Directory.h
+++ include/InteractiveToolkit/ITKCommon/FileSystem/Directory.h
@@ -406,6 +406,94 @@ namespace ITKCommon
                 return Directory(file.base_path);
             }
 
+            File toFile() const {
+                File result;
+                if (is_valid){
+                    
+                    result.full_path = ITKCommon::Path::getAbsolutePath(this->base_path);
+                #if defined(_WIN32)
+                    ITKCommon::StringUtil::replaceAll(&result.full_path, "\\", "/");
+                #endif
+                    {
+                        auto tokens = ITKCommon::StringUtil::tokenizer(result.full_path, "/");
+                        result.name = tokens[tokens.size()-1];
+                    }
+                    result.base_path = result.full_path.substr(0,result.full_path.length() - result.name.length());
+                    
+                    result.isDirectory = true;
+                    result.isFile = false;
+
+#if defined(_WIN32)
+                    std::wstring path_w;
+                    if (StringUtil::endsWith(result.full_path, "/"))
+                        path_w = StringUtil::string_to_WString(result.full_path.substr(0, path_.length() - 1));
+                    else
+                        path_w = StringUtil::string_to_WString(result.full_path);
+                    WIN32_FIND_DATAW findfiledata;
+                    HANDLE hFind = FindFirstFileW((LPCWSTR)path_w.c_str(), &findfiledata);
+                    if (hFind != INVALID_HANDLE_VALUE)
+                    {
+                        result.isDirectory = (findfiledata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) != 0;
+                        result.isFile = !result.isDirectory;
+                        //fileInfo.name = ITKCommon::StringUtil::wString_to_String(findfiledata.cFileName);
+                        //fileInfo.full_path = fileInfo.base_path + fileInfo.name;
+                        if (result.isDirectory)
+                            result.full_path += "/";
+
+                        // date processing
+                        SYSTEMTIME stUTC; //, stLocal;
+                        FileTimeToSystemTime(&findfiledata.ftLastWriteTime, &stUTC);
+                        // SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+                        result.lastWriteTime = Date::FromSystemTime_win32(stUTC);
+
+                        FileTimeToSystemTime(&findfiledata.ftCreationTime, &stUTC);
+                        // SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+                        result.creationTime = Date::FromSystemTime_win32(stUTC);
+
+                        result.size =
+                            ((uint64_t)findfiledata.nFileSizeHigh << 32) | (uint64_t)findfiledata.nFileSizeLow & UINT64_C(0xffffffff);
+
+                        FindClose(hFind);
+                    }
+#elif defined(__APPLE__) || defined(__linux__)
+
+                    // stat directory
+                    {
+                        struct statx sb;
+                        int dirfd = AT_FDCWD;
+                        // unsigned int mask = STATX_ALL;
+                        unsigned int mask = STATX_MODE | STATX_MTIME | STATX_SIZE | STATX_BTIME;
+                        int flags = AT_SYMLINK_NOFOLLOW;
+                        bool stat_success = statx(dirfd, result.full_path.c_str(), flags, mask, &sb) == 0;
+                        if (stat_success){
+                            result.isDirectory = sb.stx_mode & S_IFDIR;
+                            result.isFile = !result.isDirectory;
+
+                            if (result.isDirectory)
+                                result.full_path += "/";
+
+                            // date processing
+                            result.lastWriteTime = Date::FromUnixTimestampUTC(
+                                sb.stx_mtime.tv_sec,
+                                sb.stx_mtime.tv_nsec);
+
+                            result.creationTime = Date::FromUnixTimestampUTC(
+                                sb.stx_btime.tv_sec,
+                                sb.stx_btime.tv_nsec);
+
+                            result.size = (uint64_t)sb.stx_size;
+                        }
+
+                    }
+#endif
+
+                    if (!ITKCommon::StringUtil::endsWith(result.full_path, "/"))
+                        result.full_path += "/";
+                }
+                
+                return result;
+            }
+
         private:
             std::string base_path;
             bool is_valid;
