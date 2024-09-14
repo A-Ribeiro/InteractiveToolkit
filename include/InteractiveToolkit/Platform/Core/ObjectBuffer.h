c5fbe6fdf471f6304305aec2b6a7ebeb4bedf376
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-09-14 19:45:19 -0300

    feat: new ObjectBuffer with rvalue attribution

--- include/InteractiveToolkit/Platform/Core/ObjectBuffer.h
+++ include/InteractiveToolkit/Platform/Core/ObjectBuffer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//#include "../platform_common.h"
+// #include "../platform_common.h"
 #include "../../common.h"
 #include "../Mutex.h"
 #include "../AutoLock.h"
@@ -8,103 +8,158 @@
 #include "../../ITKCommon/ITKAbort.h"
 #include "../../ITKCommon/Memory.h"
 
-namespace Platform {
+namespace Platform
+{
 
     //
     // This Object Buffer have the default align to 32 bytes
     //   supporting 256 bits instructions ( All SSE and AVX/AVX2 ).
     //
-	class ObjectBuffer {
+    class ObjectBuffer
+    {
 
-		//private copy constructores, to avoid copy...
-        ObjectBuffer(const ObjectBuffer& v){}
-        void operator=(const ObjectBuffer& v){}
+        bool constructed_from_external_buffer;
+        Platform::Mutex mutex;
 
-		ObjectBuffer(ObjectBuffer && v){}
-        void operator=(ObjectBuffer && v){}
-
-		bool constructed_from_external_buffer;
-		Platform::Mutex mutex;
-	public:
-		uint8_t *data;
-		int64_t alloc_size;
-		int64_t size;
+    public:
+        uint8_t *data;
+        int64_t alloc_size;
+        int64_t size;
         int align;
 
-		ObjectBuffer(uint8_t *_data, int64_t _size, int _align = 32) {
-			data = _data;
-			size = _size;
-            align = _align;
-			alloc_size = 0;
-			constructed_from_external_buffer = true;
-		}
+        ObjectBuffer(uint8_t *_data, int64_t _size, int _align = 32, bool force_copy = false)
+        {
+            if (force_copy){
+                data = nullptr;
+                size = 0;
+                alloc_size = 0;
+                align = 32;
+                constructed_from_external_buffer = false;
 
-		ObjectBuffer() {
-			data = nullptr;
-			size = 0;
-			alloc_size = 0;
+                setSize(_size, _align);
+                memcpy(data, _data, _size);
+
+            } else {
+                data = _data;
+                size = _size;
+                align = _align;
+                alloc_size = 0;
+                constructed_from_external_buffer = true;
+            }
+        }
+
+        ObjectBuffer()
+        {
+            data = nullptr;
+            size = 0;
+            alloc_size = 0;
             align = 32;
-			constructed_from_external_buffer = false;
-		}
+            constructed_from_external_buffer = false;
+        }
 
-		~ObjectBuffer() {
-			free();
-		}
+        // copy construction
+        ObjectBuffer(const ObjectBuffer &v)
+        {
+            *this = v;
+        }
+        void operator=(const ObjectBuffer &v)
+        {
+            Platform::AutoLock _lovk_self(&mutex);
+            // only to clean the vars ref
+            if (constructed_from_external_buffer)
+                free();
+            setSize(v.size, v.align);
+            memcpy(data, v.data, size);
+        }
+        ObjectBuffer(ObjectBuffer &v)
+        {
+            *this = v;
+        }
+        void operator=(ObjectBuffer &v)
+        {
+            Platform::AutoLock _lovk_v(&v.mutex);
+            *this = *(const ObjectBuffer*)&v;
+        }
 
-		ObjectBuffer* setSize(int64_t _size, int _align = 32) {
+        // rvalue attribution
+        ObjectBuffer(ObjectBuffer &&v)
+        {
+            *this = std::move(v);
+        }
+        void operator=(ObjectBuffer &&v)
+        {
+            Platform::AutoLock _lovk_self(&mutex);
+            Platform::AutoLock _lovk_v(&v.mutex);
+
+            if (v.constructed_from_external_buffer){
+                *this = v;
+            } else {
+                this->data = v.data;
+                this->size = v.size;
+                this->alloc_size = v.alloc_size;
+                this->align = v.align;
+                this->constructed_from_external_buffer = v.constructed_from_external_buffer;
+
+                v.data = nullptr;
+                v.size = 0;
+                v.alloc_size = 0;
+                v.align = 32;
+                v.constructed_from_external_buffer = false;
+            }
+
+        }
+
+        ~ObjectBuffer()
+        {
+            free();
+        }
+
+        ObjectBuffer *setSize(int64_t _size, int _align = 32)
+        {
 
             ITK_ABORT(constructed_from_external_buffer, "Cannot resize a buffer object constructed from an external buffer.\n");
 
-			Platform::AutoLock autoLock(&mutex);
-			
-			if (_size == size)
-				return this;
-			
-			if (_size > alloc_size) {
-				free();
-				data = (uint8_t*)ITKCommon::Memory::malloc(_size, _align);
-				alloc_size = _size;
+            Platform::AutoLock autoLock(&mutex);
+
+            if (_size == size && _align == align)
+                return this;
+
+            if (_size > alloc_size || _align != align)
+            {
+                free();
+                data = (uint8_t *)ITKCommon::Memory::malloc(_size, _align);
+                alloc_size = _size;
                 align = _align;
-				//constructed_from_external_buffer = false;
-			}
+                // constructed_from_external_buffer = false;
+            }
 
-			size = _size;
-			return this;
-		}
-
-        ObjectBuffer* copy(const ObjectBuffer* src) {
-			Platform::AutoLock autoLock(&mutex);
-
-            setSize(src->size, src->align);
-            memcpy(data, src->data,size);
-
+            size = _size;
             return this;
         }
 
-        ObjectBuffer* copy(ObjectBuffer* src) {
-			Platform::AutoLock autoLockSelf(&mutex);
-            Platform::AutoLock autoLockOther(&src->mutex);
+        ObjectBuffer clone()
+        {
+            if (constructed_from_external_buffer)
+                return ObjectBuffer(data, size, align);
+            else
+                return *this;
+        }
 
-            setSize(src->size, src->align);
-            memcpy(data, src->data,size);
+        ObjectBuffer *free()
+        {
+            Platform::AutoLock autoLock(&mutex);
+            if (!constructed_from_external_buffer && data != nullptr)
+            {
+                ITKCommon::Memory::free(data);
+            }
+
+            data = nullptr;
+            size = 0;
+            alloc_size = 0;
+            align = 32;
+            constructed_from_external_buffer = false;
 
             return this;
         }
-
-		ObjectBuffer* free() {
-			Platform::AutoLock autoLock(&mutex);
-			if (!constructed_from_external_buffer && data != nullptr) {
-				ITKCommon::Memory::free(data);
-			}
-
-			data = nullptr;
-			size = 0;
-			alloc_size = 0;
-			constructed_from_external_buffer = false;
-
-			return this;
-		}
-
-	};
+    };
 }
-
