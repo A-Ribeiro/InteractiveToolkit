98cfbc7d4d932c8bce9b1e6cd5224952635e555f
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2024-11-04 14:10:36 -0300

    changed apple to use stat64

--- include/InteractiveToolkit/ITKCommon/FileSystem/Directory.h
+++ include/InteractiveToolkit/ITKCommon/FileSystem/Directory.h
@@ -258,14 +258,14 @@ namespace ITKCommon
                         return;
 
                     // skip . and ..
-                    struct stat sb;
+                    struct stat64 sb;
                     bool stat_success = false;
 
                     if (next_valid)
                     {
 
                         fileInfo.full_path = fileInfo.base_path + entry->d_name;
-                        stat_success = stat(fileInfo.full_path.c_str(), &sb) == 0;
+                        stat_success = stat64(fileInfo.full_path.c_str(), &sb) == 0;
 
                         // read next until a valid stat file stated
                         while (next_valid && !stat_success)
@@ -276,7 +276,7 @@ namespace ITKCommon
                             if (next_valid)
                             {
                                 fileInfo.full_path = fileInfo.base_path + entry->d_name;
-                                stat_success = stat(fileInfo.full_path.c_str(), &sb) == 0;
+                                stat_success = stat64(fileInfo.full_path.c_str(), &sb) == 0;
                             }
                         }
                     }
@@ -291,7 +291,7 @@ namespace ITKCommon
                         if (next_valid)
                         {
                             fileInfo.full_path = fileInfo.base_path + entry->d_name;
-                            stat_success = stat(fileInfo.full_path.c_str(),&sb) == 0;
+                            stat_success = stat64(fileInfo.full_path.c_str(),&sb) == 0;
 
                             // read next until a valid stat file stated
                             while (next_valid && !stat_success)
@@ -302,7 +302,7 @@ namespace ITKCommon
                                 if (next_valid)
                                 {
                                     fileInfo.full_path = fileInfo.base_path + entry->d_name;
-                                    stat_success = stat(fileInfo.full_path.c_str(), &sb) == 0;
+                                    stat_success = stat64(fileInfo.full_path.c_str(), &sb) == 0;
                                 }
                             }
                         }
@@ -327,13 +327,12 @@ namespace ITKCommon
 
                         // date processing
                         fileInfo.lastWriteTime = Date::FromUnixTimestampUTC(
-                            sb.st_mtim.tv_sec,
-                            sb.st_mtim.tv_nsec);
+                            sb.st_mtimespec.tv_sec,
+                            sb.st_mtimespec.tv_nsec);
 
-                        fileInfo.creationTime = fileInfo.lastWriteTime;
-                        // fileInfo.creationTime = Date::FromUnixTimestampUTC(
-                        //     sb.st_btime.tv_sec,
-                        //     sb.st_btime.tv_nsec);
+                        fileInfo.creationTime = Date::FromUnixTimestampUTC(
+                            sb.st_birthtimespec.tv_sec,
+                            sb.st_birthtimespec.tv_nsec);
 
                         fileInfo.size = (uint64_t)sb.st_size;
                     }
