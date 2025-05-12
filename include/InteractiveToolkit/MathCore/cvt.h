2bb9b7aaf82223fd0918c6e40a74e962dab2afe8
Alessandro Ribeiro <alessandro.ribeiro.silva@gmail.com>
2025-05-12 16:24:21 -0300

    added sRGB to linear and vice versa in the CVT

--- include/InteractiveToolkit/MathCore/cvt.h
+++ include/InteractiveToolkit/MathCore/cvt.h
@@ -16,8 +16,8 @@
 #include "mat4.h"
 
 #if defined(_WIN32)
-#pragma warning( push )
-#pragma warning( disable : 4996)
+#pragma warning(push)
+#pragma warning(disable : 4996)
 #endif
 
 namespace MathCore
@@ -40,6 +40,16 @@ namespace MathCore
         {
             return v;
         }
+
+        static ITK_INLINE float SRGBToLinear(float c)
+        {
+            return (c < 0.04045f) ? c * 0.0773993808f : OP<float>::pow(c * 0.9478672986f + 0.0521327014f, 2.4f);
+        }
+
+        static ITK_INLINE float LinearToSRGB(float c)
+        {
+            return (c < 0.0031308f) ? c * 12.92f : 1.055f * (OP<float>::pow(c, 0.41666f)) - 0.055f;
+        }
     };
 
     template <>
@@ -58,6 +68,16 @@ namespace MathCore
             if (sscanf(aux, "%e", &result) != 1)
                 result = (float)v;
             return result;
+        }
+
+        static ITK_INLINE double SRGBToLinear(double c)
+        {
+            return (c < 0.04045) ? c * 0.0773993808 : OP<double>::pow(c * 0.9478672986 + 0.0521327014, 2.4);
+        }
+
+        static ITK_INLINE double LinearToSRGB(double c)
+        {
+            return (c < 0.0031308) ? c * 12.92 : 1.055 * (OP<double>::pow(c, 0.41666)) - 0.055;
         }
     };
 
@@ -90,6 +110,34 @@ namespace MathCore
         {
             return self_type::toVec2(v) * ((_type)1 / v.w);
         }
+
+        template <typename in_type = _type,
+                  typename std::enable_if<
+                      std::is_same<in_type, float>::value ||
+                          std::is_same<in_type, double>::value,
+                      bool>::type = true>
+        static ITK_INLINE type4 sRGBToLinear(const type4 &v) noexcept
+        {
+            return type4(
+                CVT<_type>::SRGBToLinear(v.x),
+                CVT<_type>::SRGBToLinear(v.y),
+                CVT<_type>::SRGBToLinear(v.z),
+                v.a);
+        }
+
+        template <typename in_type = _type,
+                  typename std::enable_if<
+                      std::is_same<in_type, float>::value ||
+                          std::is_same<in_type, double>::value,
+                      bool>::type = true>
+        static ITK_INLINE type4 LinearTosRGB(const type4 &v) noexcept
+        {
+            return type4(
+                CVT<_type>::LinearToSRGB(v.x),
+                CVT<_type>::LinearToSRGB(v.y),
+                CVT<_type>::LinearToSRGB(v.z),
+                v.a);
+        }
     };
 
     template <typename _type, typename _simd>
@@ -115,6 +163,32 @@ namespace MathCore
         static ITK_INLINE lower_type2 toVec2(const type3 &v) noexcept
         {
             return lower_type2(*(const lower_type2 *)&v);
+        }
+
+        template <typename in_type = _type,
+                  typename std::enable_if<
+                      std::is_same<in_type, float>::value ||
+                          std::is_same<in_type, double>::value,
+                      bool>::type = true>
+        static ITK_INLINE type3 sRGBToLinear(const type3 &v) noexcept
+        {
+            return type3(
+                CVT<_type>::SRGBToLinear(v.x),
+                CVT<_type>::SRGBToLinear(v.y),
+                CVT<_type>::SRGBToLinear(v.z));
+        }
+
+        template <typename in_type = _type,
+                  typename std::enable_if<
+                      std::is_same<in_type, float>::value ||
+                          std::is_same<in_type, double>::value,
+                      bool>::type = true>
+        static ITK_INLINE type3 LinearTosRGB(const type3 &v) noexcept
+        {
+            return type3(
+                CVT<_type>::LinearToSRGB(v.x),
+                CVT<_type>::LinearToSRGB(v.y),
+                CVT<_type>::LinearToSRGB(v.z));
         }
     };
 
@@ -178,53 +252,53 @@ namespace MathCore
         // }
 
         // // original: extractQuat
-		// static ITK_INLINE quatT toQuat(const typeMat4 &mp) noexcept
-		// {
-		// 	typeMat4 m = mp;
+        // static ITK_INLINE quatT toQuat(const typeMat4 &mp) noexcept
+        // {
+        // 	typeMat4 m = mp;
 
-		// 	// normalize rotation part
-		// 	*(lower_type3 *)&(m[0]) = OP<lower_type3>::normalize(*(lower_type3 *)&(m[0]));
-		// 	*(lower_type3 *)&(m[1]) = OP<lower_type3>::normalize(*(lower_type3 *)&(m[1]));
-		// 	*(lower_type3 *)&(m[2]) = OP<lower_type3>::normalize(*(lower_type3 *)&(m[2]));
+        // 	// normalize rotation part
+        // 	*(lower_type3 *)&(m[0]) = OP<lower_type3>::normalize(*(lower_type3 *)&(m[0]));
+        // 	*(lower_type3 *)&(m[1]) = OP<lower_type3>::normalize(*(lower_type3 *)&(m[1]));
+        // 	*(lower_type3 *)&(m[2]) = OP<lower_type3>::normalize(*(lower_type3 *)&(m[2]));
 
-		// 	_type t = (_type)1 + m.a1 + m.b2 + m.c3;
-		// 	// large enough
-		// 	if (t > (_type)0.001)
-		// 	{
-		// 		_type s = OP<_type>::sqrt(t) * (_type)2;
-		// 		return quatT((m.b3 - m.c2) / s,
-		// 					 (m.c1 - m.a3) / s,
-		// 					 (m.a2 - m.b1) / s,
-		// 					 (_type)0.25 * s);
-		// 	} // else we have to check several cases
-		// 	else if (m.a1 > m.b2 && m.a1 > m.c3)
-		// 	{
-		// 		// Column 0:
-		// 		_type s = OP<_type>::sqrt((_type)1 + m.a1 - m.b2 - m.c3) * (_type)2;
-		// 		return quatT((_type)0.25 * s,
-		// 					 (m.a2 + m.b1) / s,
-		// 					 (m.c1 + m.a3) / s,
-		// 					 (m.b3 - m.c2) / s);
-		// 	}
-		// 	else if (m.b2 > m.c3)
-		// 	{
-		// 		// Column 1:
-		// 		_type s = OP<_type>::sqrt((_type)1 + m.b2 - m.a1 - m.c3) * (_type)2;
-		// 		return quatT((m.a2 + m.b1) / s,
-		// 					 (_type)0.25 * s,
-		// 					 (m.b3 + m.c2) / s,
-		// 					 (m.c1 - m.a3) / s);
-		// 	}
-		// 	else
-		// 	{
-		// 		// Column 2:
-		// 		_type s = OP<_type>::sqrt((_type)1 + m.c3 - m.a1 - m.b2) * (_type)2;
-		// 		return quatT((m.c1 + m.a3) / s,
-		// 					 (m.b3 + m.c2) / s,
-		// 					 (_type)0.25 * s,
-		// 					 (m.a2 - m.b1) / s);
-		// 	}
-		// }
+        // 	_type t = (_type)1 + m.a1 + m.b2 + m.c3;
+        // 	// large enough
+        // 	if (t > (_type)0.001)
+        // 	{
+        // 		_type s = OP<_type>::sqrt(t) * (_type)2;
+        // 		return quatT((m.b3 - m.c2) / s,
+        // 					 (m.c1 - m.a3) / s,
+        // 					 (m.a2 - m.b1) / s,
+        // 					 (_type)0.25 * s);
+        // 	} // else we have to check several cases
+        // 	else if (m.a1 > m.b2 && m.a1 > m.c3)
+        // 	{
+        // 		// Column 0:
+        // 		_type s = OP<_type>::sqrt((_type)1 + m.a1 - m.b2 - m.c3) * (_type)2;
+        // 		return quatT((_type)0.25 * s,
+        // 					 (m.a2 + m.b1) / s,
+        // 					 (m.c1 + m.a3) / s,
+        // 					 (m.b3 - m.c2) / s);
+        // 	}
+        // 	else if (m.b2 > m.c3)
+        // 	{
+        // 		// Column 1:
+        // 		_type s = OP<_type>::sqrt((_type)1 + m.b2 - m.a1 - m.c3) * (_type)2;
+        // 		return quatT((m.a2 + m.b1) / s,
+        // 					 (_type)0.25 * s,
+        // 					 (m.b3 + m.c2) / s,
+        // 					 (m.c1 - m.a3) / s);
+        // 	}
+        // 	else
+        // 	{
+        // 		// Column 2:
+        // 		_type s = OP<_type>::sqrt((_type)1 + m.c3 - m.a1 - m.b2) * (_type)2;
+        // 		return quatT((m.c1 + m.a3) / s,
+        // 					 (m.b3 + m.c2) / s,
+        // 					 (_type)0.25 * s,
+        // 					 (m.a2 - m.b1) / s);
+        // 	}
+        // }
     };
 
     template <typename _type, typename _simd>
@@ -258,53 +332,53 @@ namespace MathCore
         // }
 
         // // original: extractQuat
-		// static ITK_INLINE quatT toQuat(const typeMat3 &mp) noexcept
-		// {
-		// 	typeMat3 m = mp;
+        // static ITK_INLINE quatT toQuat(const typeMat3 &mp) noexcept
+        // {
+        // 	typeMat3 m = mp;
 
-		// 	// normalize rotation part
-		// 	m[0] = OP<type3>::normalize(m[0]);
-		// 	m[1] = OP<type3>::normalize(m[1]);
-		// 	m[2] = OP<type3>::normalize(m[2]);
+        // 	// normalize rotation part
+        // 	m[0] = OP<type3>::normalize(m[0]);
+        // 	m[1] = OP<type3>::normalize(m[1]);
+        // 	m[2] = OP<type3>::normalize(m[2]);
 
-		// 	_type t = (_type)1 + m.a1 + m.b2 + m.c3;
-		// 	// large enough
-		// 	if (t > (_type)0.001)
-		// 	{
-		// 		_type s = OP<_type>::sqrt(t) * (_type)2;
-		// 		return quatT((m.b3 - m.c2) / s,
-		// 					 (m.c1 - m.a3) / s,
-		// 					 (m.a2 - m.b1) / s,
-		// 					 (_type)0.25 * s);
-		// 	} // else we have to check several cases
-		// 	else if (m.a1 > m.b2 && m.a1 > m.c3)
-		// 	{
-		// 		// Column 0:
-		// 		_type s = OP<_type>::sqrt((_type)1 + m.a1 - m.b2 - m.c3) * (_type)2;
-		// 		return quatT((_type)0.25 * s,
-		// 					 (m.a2 + m.b1) / s,
-		// 					 (m.c1 + m.a3) / s,
-		// 					 (m.b3 - m.c2) / s);
-		// 	}
-		// 	else if (m.b2 > m.c3)
-		// 	{
-		// 		// Column 1:
-		// 		_type s = OP<_type>::sqrt((_type)1 + m.b2 - m.a1 - m.c3) * (_type)2;
-		// 		return quatT((m.a2 + m.b1) / s,
-		// 					 (_type)0.25 * s,
-		// 					 (m.b3 + m.c2) / s,
-		// 					 (m.c1 - m.a3) / s);
-		// 	}
-		// 	else
-		// 	{
-		// 		// Column 2:
-		// 		_type s = OP<_type>::sqrt((_type)1 + m.c3 - m.a1 - m.b2) * (_type)2;
-		// 		return quatT((m.c1 + m.a3) / s,
-		// 					 (m.b3 + m.c2) / s,
-		// 					 (_type)0.25 * s,
-		// 					 (m.a2 - m.b1) / s);
-		// 	}
-		// }
+        // 	_type t = (_type)1 + m.a1 + m.b2 + m.c3;
+        // 	// large enough
+        // 	if (t > (_type)0.001)
+        // 	{
+        // 		_type s = OP<_type>::sqrt(t) * (_type)2;
+        // 		return quatT((m.b3 - m.c2) / s,
+        // 					 (m.c1 - m.a3) / s,
+        // 					 (m.a2 - m.b1) / s,
+        // 					 (_type)0.25 * s);
+        // 	} // else we have to check several cases
+        // 	else if (m.a1 > m.b2 && m.a1 > m.c3)
+        // 	{
+        // 		// Column 0:
+        // 		_type s = OP<_type>::sqrt((_type)1 + m.a1 - m.b2 - m.c3) * (_type)2;
+        // 		return quatT((_type)0.25 * s,
+        // 					 (m.a2 + m.b1) / s,
+        // 					 (m.c1 + m.a3) / s,
+        // 					 (m.b3 - m.c2) / s);
+        // 	}
+        // 	else if (m.b2 > m.c3)
+        // 	{
+        // 		// Column 1:
+        // 		_type s = OP<_type>::sqrt((_type)1 + m.b2 - m.a1 - m.c3) * (_type)2;
+        // 		return quatT((m.a2 + m.b1) / s,
+        // 					 (_type)0.25 * s,
+        // 					 (m.b3 + m.c2) / s,
+        // 					 (m.c1 - m.a3) / s);
+        // 	}
+        // 	else
+        // 	{
+        // 		// Column 2:
+        // 		_type s = OP<_type>::sqrt((_type)1 + m.c3 - m.a1 - m.b2) * (_type)2;
+        // 		return quatT((m.c1 + m.a3) / s,
+        // 					 (m.b3 + m.c2) / s,
+        // 					 (_type)0.25 * s,
+        // 					 (m.a2 - m.b1) / s);
+        // 	}
+        // }
     };
 
     template <typename _type, typename _simd>
@@ -389,11 +463,10 @@ namespace MathCore
         //                     (_type)2 * (xy + wz), (_type)1 - (_type)2 * (x2 + z2), (_type)2 * (yz - wx),
         //                     (_type)2 * (xz - wy), (_type)2 * (yz + wx), (_type)1 - (_type)2 * (x2 + y2));
         // }
-
     };
 
 }
 
 #if defined(_WIN32)
-#pragma warning( pop )
+#pragma warning(pop)
 #endif
